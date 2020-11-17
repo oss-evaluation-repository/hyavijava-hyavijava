b480e1c8df98e16d419ccab7e50552906a31ae2a
hyavijava <72743114+hyavijava@users.noreply.github.com>
2020-11-17 10:41:16 +0800

    Create HyavijavaTest.java
    hyavijava first test

---
+++ src/test/java/com/vmware/vim25/HyavijavaTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public Class HyavijavaTest {
+    @Test
+    public void hello_world_test_of_hyavijava() {
+    assertEquals("1","1");
+    }
+}
